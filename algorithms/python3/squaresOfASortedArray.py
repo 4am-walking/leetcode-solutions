8737a81202f1ac7a2028dd27e9dd605d54d7255b
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-11 21:34:34 -0400

    Update squaresOfASortedArray.py

--- algorithms/python3/squaresOfASortedArray.py
+++ algorithms/python3/squaresOfASortedArray.py
@@ -1,4 +1,3 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        resultArray = sorted(num * num for num in nums)
-        return resultArray
+         return sorted(num * num for num in nums)
