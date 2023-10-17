f69d87648dc902de51684fc677fc42d600650a9e
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-17 15:06:00 -0500

    Create removeDuplicatesFromSortedArray.c

---
+++ algorithms/c/removeDuplicatesFromSortedArray.c
@@ -0,0 +1,10 @@
+int removeDuplicates(int* nums, int numsSize){
+    int count = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (i == 0 || nums[i] != nums[i - 1]) {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    return count;
+}
